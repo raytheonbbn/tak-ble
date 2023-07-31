1773d199715c85b73a7d300777d52f219a3f7881
Edward Lu <edward.lu@raytheon.com>
2023-07-31 13:59:35 -0400

    Refactored code to move HTTPServerThread to its own class

--- ble-forwarder-atak/app/src/main/java/com/atakmap/android/ble_forwarder/MainDropDownReceiver.java
+++ ble-forwarder-atak/app/src/main/java/com/atakmap/android/ble_forwarder/MainDropDownReceiver.java
@@ -41,6 +41,7 @@ import android.widget.TextView;
 import com.atak.plugins.impl.PluginLayoutInflater;
 import com.atakmap.android.ble_forwarder.plugin.R;
 import com.atakmap.android.ble_forwarder.takserver_facade.CoTServerThread;
+import com.atakmap.android.ble_forwarder.takserver_facade.HttpServerThread;
 import com.atakmap.android.ble_forwarder.util.BLEUtil;
 import com.atakmap.android.ble_forwarder.util.DateUtil;
 import com.atakmap.android.dropdown.DropDown;
@@ -142,8 +143,6 @@ public class MainDropDownReceiver extends DropDownReceiver
     public static Queue<String> newCotQueue = new ArrayBlockingQueue<>(1000);
     public static Queue<String> newHttpQueue = new ArrayBlockingQueue<>(1000);
 
-    public static Queue<String> outgoingHttpQueue = new ArrayBlockingQueue<>(1000);
-
     public static Queue<String> peripheralLogMessages = new ArrayBlockingQueue<>(1000);
     public static Queue<String> centralLogMessages = new ArrayBlockingQueue<>(1000);
 
@@ -154,8 +153,6 @@ public class MainDropDownReceiver extends DropDownReceiver
     public static final int READ_SIZE = 20;
 
     public static String receivedCot = "";
-
-    ServerSocket serverSocket = null;
 
     /**************************** CONSTRUCTOR *****************************/
 
@@ -276,7 +273,7 @@ public class MainDropDownReceiver extends DropDownReceiver
         this.cotServer = new CoTServerThread(SERVERPORT1, this, peripheralLogMessages);
         this.cotServerThread = new Thread(cotServer);
         this.cotServerThread.start();
-        this.httpServerThread = new Thread(new HttpServerThread(8080));
+        this.httpServerThread = new Thread(new HttpServerThread(8080, peripheralLogMessages));
         httpServerThread.start();
         this.cotdequeuerThread = new Thread(new NewCotDequeuer());
         this.cotdequeuerThread.start();
@@ -501,112 +498,6 @@ public class MainDropDownReceiver extends DropDownReceiver
                 }
             }
         }
-    }
-
-    class HttpServerThread implements Runnable {
-
-        private final String TAG = HttpServerThread.class.getSimpleName();
-
-        int port;
-
-        public HttpServerThread(int port) {
-            this.port = port;
-        }
-
-        public void run() {
-
-            Log.d(TAG, "Running http server thread.");
-
-            Socket socket = null;
-            try {
-                serverSocket = new ServerSocket(port);
-            } catch (IOException e) {
-                e.printStackTrace();
-                return;
-            }
-
-            Log.d(TAG, "Created server socket on port " + port);
-
-            try {
-
-                socket = serverSocket.accept();
-
-                Log.d(TAG, "Accepted server connection on port " + port);
-
-                BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
-
-                while (!Thread.currentThread().isInterrupted()) {
-
-                    // input processing
-
-                    //Log.d(TAG, "Trying to read string from connection...");
-                    try {
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                        StringBuilder result = new StringBuilder();
-                        String line;
-                        while((line = reader.readLine()) != null) {
-                            result.append(line);
-                        }
-                        if (!result.toString().isEmpty()) {
-                            Log.d(TAG, "Read string from http connection: " + result.toString());
-                            if (result.toString().startsWith("GET /Marti/api/clientEndPoints")) {
-                                outgoingHttpQueue.add(
-                                        "HTTP/1.1 200 OK" + "\r\n" +
-                                        "Date: " + DateUtil.formatHttpDate(System.currentTimeMillis()) + "\r\n" +
-                                        "Content-Type: application/json; charset=utf-8" + "\r\n" +
-                                        "Keep-Alive: " + "timeout=60" + "\r\n" +
-                                        "Transfer-Encoding: " + "chunked" + "\r\n" +
-                                        "Connection: " + "keep-alive" + "\r\n" +
-                                        "\r\n" +
-                                        "{\"version\":\"3\",\"type\":\"com.bbn.marti.remote.ClientEndpoint\",\"data\":[]," +
-                                                "\"nodeId\":\"e6ec3550334a41aeb08b06e9578ea212\"}" + "\r\n\r\n");
-                            } else if (result.toString().startsWith("GET /Marti/api/version/config")) {
-                                outgoingHttpQueue.add(
-                                        "HTTP/1.1 200 OK" + "\r\n" +
-                                        "Date: " + DateUtil.formatHttpDate(System.currentTimeMillis()) + "\r\n" +
-                                        "Content-Type: application/json; charset=utf-8" + "\r\n" +
-                                        "Keep-Alive: " + "timeout=60" + "\r\n" +
-                                        "Transfer-Encoding: " + "chunked" + "\r\n" +
-                                        "Connection: " + "keep-alive" + "\r\n" +
-                                        "\r\n" +
-                                        "{\"version\":\"3\",\"type\":\"ServerConfig\",\"data\":{\"version\":\"4.5.38-RELEASE\",\"api\":\"3\"," +
-                                                "\"hostname\":\"localhost\"},\"nodeId\":\"e6ec3550334a41aeb08b06e9578ea212\"}" + "\r\n\r\n");
-                            }
-                        }
-                    } catch (Exception e) {
-                        Log.e(TAG, "error reading message from input stream", e);
-                    }
-
-                    // output processing
-
-                    if (!outgoingHttpQueue.isEmpty()) {
-                        OutputStream os = socket.getOutputStream();
-                        while (!outgoingHttpQueue.isEmpty()) {
-                            String outgoingHttp = outgoingHttpQueue.poll();
-                            if (outgoingHttp != null) {
-                                os.write(outgoingHttp.getBytes(StandardCharsets.UTF_8));
-                                peripheralLogMessages.add("Wrote HTTP received over BLE to local ATAK: " + outgoingHttp);
-                            }
-                        }
-                    }
-
-                    Thread.sleep(1000);
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    Log.d(TAG, "Closing server socket...");
-                    serverSocket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
     }
 
     /**
---
+++ ble-forwarder-atak/app/src/main/java/com/atakmap/android/ble_forwarder/takserver_facade/HttpServerThread.java
@@ -0,0 +1,128 @@
+package com.atakmap.android.ble_forwarder.takserver_facade;
+
+import com.atakmap.android.ble_forwarder.util.DateUtil;
+import com.atakmap.coremap.log.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class HttpServerThread implements Runnable {
+
+    private final String TAG = HttpServerThread.class.getSimpleName();
+
+    int port;
+
+    ServerSocket serverSocket = null;
+
+    public static Queue<String> outgoingHttpQueue = new ArrayBlockingQueue<>(1000);
+    public Queue<String> peripheralLogMessages;
+
+    public HttpServerThread(int port,
+                            Queue<String> peripheralLogMessages) {
+        this.port = port;
+        this.peripheralLogMessages = peripheralLogMessages;
+    }
+
+    public void run() {
+
+        Log.d(TAG, "Running http server thread.");
+
+        Socket socket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Log.d(TAG, "Created server socket on port " + port);
+
+        try {
+
+            socket = serverSocket.accept();
+
+            Log.d(TAG, "Accepted server connection on port " + port);
+
+            BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+
+            while (!Thread.currentThread().isInterrupted()) {
+
+                // input processing
+
+                //Log.d(TAG, "Trying to read string from connection...");
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+                    while((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+                    if (!result.toString().isEmpty()) {
+                        Log.d(TAG, "Read string from http connection: " + result.toString());
+                        if (result.toString().startsWith("GET /Marti/api/clientEndPoints")) {
+                            outgoingHttpQueue.add(
+                                    "HTTP/1.1 200 OK" + "\r\n" +
+                                            "Date: " + DateUtil.formatHttpDate(System.currentTimeMillis()) + "\r\n" +
+                                            "Content-Type: application/json; charset=utf-8" + "\r\n" +
+                                            "Keep-Alive: " + "timeout=60" + "\r\n" +
+                                            "Transfer-Encoding: " + "chunked" + "\r\n" +
+                                            "Connection: " + "keep-alive" + "\r\n" +
+                                            "\r\n" +
+                                            "{\"version\":\"3\",\"type\":\"com.bbn.marti.remote.ClientEndpoint\",\"data\":[]," +
+                                            "\"nodeId\":\"e6ec3550334a41aeb08b06e9578ea212\"}" + "\r\n\r\n");
+                        } else if (result.toString().startsWith("GET /Marti/api/version/config")) {
+                            outgoingHttpQueue.add(
+                                    "HTTP/1.1 200 OK" + "\r\n" +
+                                            "Date: " + DateUtil.formatHttpDate(System.currentTimeMillis()) + "\r\n" +
+                                            "Content-Type: application/json; charset=utf-8" + "\r\n" +
+                                            "Keep-Alive: " + "timeout=60" + "\r\n" +
+                                            "Transfer-Encoding: " + "chunked" + "\r\n" +
+                                            "Connection: " + "keep-alive" + "\r\n" +
+                                            "\r\n" +
+                                            "{\"version\":\"3\",\"type\":\"ServerConfig\",\"data\":{\"version\":\"4.5.38-RELEASE\",\"api\":\"3\"," +
+                                            "\"hostname\":\"localhost\"},\"nodeId\":\"e6ec3550334a41aeb08b06e9578ea212\"}" + "\r\n\r\n");
+                        }
+                    }
+                } catch (Exception e) {
+                    Log.e(TAG, "error reading message from input stream", e);
+                }
+
+                // output processing
+
+                if (!outgoingHttpQueue.isEmpty()) {
+                    OutputStream os = socket.getOutputStream();
+                    while (!outgoingHttpQueue.isEmpty()) {
+                        String outgoingHttp = outgoingHttpQueue.poll();
+                        if (outgoingHttp != null) {
+                            os.write(outgoingHttp.getBytes(StandardCharsets.UTF_8));
+                            peripheralLogMessages.add("Wrote HTTP received over BLE to local ATAK: " + outgoingHttp);
+                        }
+                    }
+                }
+
+                Thread.sleep(1000);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                Log.d(TAG, "Closing server socket...");
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
